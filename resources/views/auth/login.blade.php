6ff3437b37deea475e79b05c28a6b3fbd448c842
sivattha.yun@live.com <sivattha.yun@live.com>
2022-09-18 20:24:25 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>User Login | Vdoo ERP</title>
+    <title>User Login | CMS System</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
   
